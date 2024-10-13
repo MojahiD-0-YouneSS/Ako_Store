324585463feb3eb4529cf33e02e6e640c5b30194
Mojahid Youness <mojahidyouness0@gmail.com>
2024-10-13 16:38:36 +0100

    gsdggsrgkokop

--- Ako_store/settings.py
+++ Ako_store/settings.py
@@ -117,15 +117,15 @@ WSGI_APPLICATION = 'Ako_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
-    
-}
+'''    'default': dj_database_url.config(conn_max_age=600)
 '''
+DATABASES = {
 'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }'''
+    }
+    
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
